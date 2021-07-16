52230caec35cbbf983685ac7761662430786b7f6
CristobalHdz <cristobalhdzch@gmail.com>
2021-07-16 16:39:59 -0600

    Fix HTML reference and completed the README file

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,19 @@
-// https://w3collective.com/random-password-generator-javascript/
-
 // DOM Elements
 var generateBtn = document.querySelector("#generate");
-
-/* Moved from "writePassword" function for a cleaner code */
 var passwordText = document.querySelector("#password");
-
 var lengthElement = document.getElementById("length");
 var lowercaseElement = document.getElementById("lowercase");
 var uppercaseElement = document.getElementById("uppercase");
 var numberElement = document.getElementById("numbers");
 var symbolElement = document.getElementById("symbols");
 
+//Password characters assigned to checkboxes
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = lowercase.toLocaleUpperCase();
 var numbers = "0123456789"
 var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 
-
-// generateBtn.addEventListener("click", writePassword);
-/* This event listener can be reduced into a annonymous arrow function */
-
-// Write password to the #password input
 generateBtn.addEventListener("click", () => {
 
   // Input Rules
@@ -31,46 +22,38 @@ generateBtn.addEventListener("click", () => {
     alert("Please select a character checkbox and a value of 128 or less")
     return;
   };
-
   //No checkboxes & value less than 8
   if ((lengthElement.value < 8) && (lowercaseElement.checked == false && uppercaseElement.checked == false && numberElement.checked == false && symbolElement.checked == false)) {
     alert("Please select a character checkbox and a value of 8 or more")
     return;
   };
-
   //Value less than 8
   if (lengthElement.value < 8) {
     alert("Please select a value of 8 or more")
     return;
   };
-
   //Value more than 128
   if (lengthElement.value > 128) {
     alert("Please select a value of 128 or less")
     return;
   };
-
   //No checkboxes
   if (lowercaseElement.checked == false && uppercaseElement.checked == false && numberElement.checked == false && symbolElement.checked == false) {
     alert("Please select a character checkbox")
     return;
   };
 
+  //Checkbox event listener
   var passwordCharacters = "";
-  //Tells if boxes are checked and what to do
   lowercaseElement.checked ? (passwordCharacters += lowercase) : "";
   uppercaseElement.checked ? (passwordCharacters += uppercase) : "";
   numberElement.checked ? (passwordCharacters += numbers) : "";
   symbolElement.checked ? (passwordCharacters += symbols) : "";
 
-
-
-  /* Merged both lines to clean the code*/
-  // var password = generatePassword();
-  // passwordText.value = password;
   passwordText.value = generatePassword(lengthElement.value, passwordCharacters);
 });
 
+//Password character loop
 var generatePassword = (lengthElement, passwordCharacters) => {
   let password = "";
   for (let i = 0; i < lengthElement; i++) {
---
+++ README.md
@@ -0,0 +1,47 @@
+ # 03 Jacascript
+
+
+# Section 01: General Information
+
+Third code Boot Camp assignment.
+</br>
+</br>
+`Password Generator`
+</br>
+</br>
+ by Cristobal Hernandez
+ 
+ ## Published link
+ https://cristobalhdz.github.io/PasswordGenerator_ThirdHW/
+
+# Section 02: General Information
+
+The assignment for this week is to make a random password generator. This week we have been given some of the code and we have to work over it to make a functioning random password generator.
+
+# Section 03: Code I changed and the explanation on why
+
+Line 3: `var passwordText = document.querySelector("#password")`
+Explanation: Taken out of the function because the DOM variable can be global without problems. If the variable was only supposed to be locally used, then it hsould stay inside the function. The code looks, and is, cleaner with all the variables together.
+
+Line 17: `generateBtn.addEventListener("click", writePassword)` to `generateBtn.addEventListener("click", () => {};)`
+Explanation: The code can be reduced by assining the annonymous function directly. The changed code is cleaner and easier to read and edit. 
+
+Line 53: `var password = generatePassword()` & `passwordText.value = password;`  to `passwordText.value = generatePassword();`
+Explanation: The removal of the "password" gives us the oportunity to shrten the code and make it less complicated to read.
+
+#  Section 04: My experience
+
+I had a really rought time with the given code. The last two of the three changes I made were so I could work with order. I had to see what password was equal to so I could apply it to the value so that is why I decided just ot merge some code to make it easier to read and work on.
+
+The loop for the characters to position themselves depending on the length was pretty fun to search on the web. I used the one from the references because it was the cleanest out of the ones I looked at. 
+</br>
+The refference had the `.charAt()` which gave me the oportunity to search what it does and how it can be applied inside of the code.
+</br>
+I did add some filters just for fun to make the code as complete as possible. Let's call it a "fool-proof" way of telling the user what is missing for the code to work.
+
+# References
+`Code help reference`
+https://w3collective.com/random-password-generator-javascript/
+
+`.charAt() method`
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
